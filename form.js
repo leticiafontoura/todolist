aff50196fa914c4555307b8a1cd7e2b9ad9d6772
Leticia Fontoura <leticia.fontoura@live.com>
2021-08-24 19:07:56 -0300

    first try

---
+++ form.js
@@ -0,0 +1,40 @@
+const form = document.querySelector("[data-js='form']");
+const input = document.querySelector("[data-js='to-do']");
+const list = document.querySelector("[data-js='body-list']");
+console.log(list)
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("enviado");
+    list.insertAdjacentHTML("beforeend", `
+    <tr class="list-row" data-js="list-row">
+        <td class="list-item" data-js="list-item">${input.value}</td>
+        <td>
+            <button class="done-btn" data-js="done-btn" value="done">done</button>
+            <button class="del-btn" data-js="del-btn" value="delete">delete</button>
+        </td>
+    </tr>
+    `)
+
+
+    const doneBtns = Array.from(document.querySelectorAll("[data-js='done-btn']"));
+    const delBtns = Array.from(document.querySelectorAll("[data-js='del-btn']"));
+    const listItem = Array.from(document.querySelectorAll("[data-js='list-item']"));
+    const listRow = Array.from(document.querySelectorAll("[data-js='list-row']"));
+
+    for (let i = 0; i < doneBtns.length; i++) {
+        doneBtns[i].addEventListener("click", () => {
+            console.log("botao" + (1 + i))
+            listItem[i].classList.contains("linethrough") ? listItem[i].classList.remove("linethrough") : listItem[i].classList.add("linethrough");
+            doneBtns[i].innerHTML === "done" ? doneBtns[i].innerHTML = "undo" : doneBtns[i].innerHTML = "done";
+        })
+    }
+
+
+    for (let i = 0; i < delBtns.length; i++) {
+        delBtns[i].addEventListener("click", (e) => {
+            listRow[i].remove();
+        })
+    }
+
+})
