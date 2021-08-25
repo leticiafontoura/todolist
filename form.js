ca7b3812fa04424dd71e50e6ac0536fbda7b8cf7
Leticia Fontoura <leticia.fontoura@live.com>
2021-08-25 02:13:04 -0300

    adjusting localstorage

--- form.js
+++ form.js
@@ -4,6 +4,24 @@ const list = document.querySelector("[data-js='body-list']");
 console.log(list);
 
 function renderItems() {
+
+    const todoItems = JSON.parse(localStorage.getItem("items"));
+    console.log(todoItems);
+
+    for (let i = 0; i < todoItems.length; i++) {
+        list.insertAdjacentHTML("beforeend", `
+    <tr class="list-row" data-js="list-row">
+        <td class="list-item" data-js="list-item">${todoItems[i].item}</td>
+        <td>
+            <button class="done-btn" data-js="done-btn" value="done">done</button>
+            <button class="del-btn" data-js="del-btn" value="delete">delete</button>
+        </td>
+    </tr>
+    `)
+    }
+
+
+
     const doneBtns = Array.from(document.querySelectorAll("[data-js='done-btn']"));
     const delBtns = Array.from(document.querySelectorAll("[data-js='del-btn']"));
     const listItem = Array.from(document.querySelectorAll("[data-js='list-item']"));
@@ -15,7 +33,7 @@ function renderItems() {
             listItem[i].classList.contains("linethrough") ? listItem[i].classList.remove("linethrough") : listItem[i].classList.add("linethrough");
             doneBtns[i].innerHTML === "done" ? doneBtns[i].innerHTML = "undo" : doneBtns[i].innerHTML = "done";
         }
-        
+
     }
 
     for (let i = 0; i < delBtns.length; i++) {
@@ -25,18 +43,39 @@ function renderItems() {
     }
 }
 
+const putItems = JSON.parse(localStorage.getItem("items")) ? ? [];
+console.log(putItems);
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+
+    console.log(putItems.length);
+
+    if (putItems.length === 0) {
+        putItems.push({
+            id: new Date(),
+            item: input.value
+        });
+
+        console.log(putItems);
+
+        localStorage.setItem("items", JSON.stringify(putItems));
+        console.log("saiu")
+        return renderItems();
+    }
+
+    const arr2 = JSON.parse(localStorage.getItem("items"));
+    arr2.push({
+        id: new Date(),
+        item: input.value
+    });
+    localStorage.setItem("items", JSON.stringify(arr2));
+    console.log(arr2);
+
     console.log("enviado");
-    list.insertAdjacentHTML("beforeend", `
-    <tr class="list-row" data-js="list-row">
-        <td class="list-item" data-js="list-item">${input.value}</td>
-        <td>
-            <button class="done-btn" data-js="done-btn" value="done">done</button>
-            <button class="del-btn" data-js="del-btn" value="delete">delete</button>
-        </td>
-    </tr>
-    `)
+
     renderItems();
 
 })
+
+renderItems();
